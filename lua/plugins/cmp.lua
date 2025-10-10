935407dae8dd8a3b27c329df49a5a29c8d0448c7
Stefan Wellhoner <stefanwellhoner@Stefans-Mac-mini.local>
2025-10-10 06:23:44 +0200

    Updated autocomplete keymap

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -19,7 +19,7 @@ return {
 				},
 				mapping = cmp.mapping.preset.insert({
 					["<C-Space>"] = cmp.mapping.complete(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<C-y>"] = cmp.mapping.confirm({ select = true }),
 					["<Tab>"] = cmp.mapping.select_next_item(),
 					["<S-Tab>"] = cmp.mapping.select_prev_item(),
 				}),
